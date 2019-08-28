dcc9ed74682917cf7e3061a4c6ae299e8bebc6fb
alysonabril <alysonabril@pursuit.org>
2019-08-28 16:23:37 -0400

    created tab bvar controller, each tab has a view controller with tableview and selecting a cell will segue to dvc

---
+++ weatherApp/Control/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Alyson Abril on 8/28/19.
+//  Copyright © 2019 Alyson Abril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
